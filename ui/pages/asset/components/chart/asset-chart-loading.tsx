c062090237440d1479a100b08d81b99f593b5ae2
gencdurore56 <148649290+gencdurore56@users.noreply.github.com>
2025-07-09 02:33:01 -0600

    Create asset-chart-loading.tsx

---
+++ ui/pages/asset/components/chart/asset-chart-loading.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Skeleton } from '../../../../components/component-library/skeleton';
+import { Box } from '../../../../components/component-library';
+import { BorderRadius } from '../../../../helpers/constants/design-system';
+
+export const AssetChartLoading = () => (
+  <Box
+    className="asset-chart__empty-or-loading-state-container"
+    data-testid="asset-chart-loading"
+  >
+    <Skeleton
+      className="asset-chart__skeleton"
+      borderRadius={BorderRadius.LG}
+    />
+  </Box>
+);
